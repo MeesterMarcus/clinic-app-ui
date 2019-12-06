7ae8dff31dd3cded7a73c6fdb1a7aeb9249d04ca
MeesterMarcus <lorenzanamarcus@gmail.com>
2019-12-05 23:59:41 -0600

    implemented global search patients

--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -1,5 +1,5 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import { ColumnMode} from '@swimlane/ngx-datatable';
+import {Component, OnInit} from '@angular/core';
+import {ColumnMode } from '@swimlane/ngx-datatable';
 import {AddPatientDialogComponent} from './add-patient-dialog/add-patient-dialog.component';
 import {MatDialog} from '@angular/material/dialog';
 import {PatientService} from '../services/patient.service';
@@ -11,18 +11,20 @@ import {Router} from '@angular/router';
   styleUrls: ['./patients.component.css']
 })
 export class PatientsComponent implements OnInit {
-
   loadingIndicator = true;
   reorderable = true;
   ColumnMode = ColumnMode;
 
 
-  rows: {};
+  rows = [];
+  temp = [];
+  columnsWithSearch: string[] = [];
 
   constructor(public dialog: MatDialog, private patientService: PatientService, private router: Router) { }
 
   ngOnInit() {
     this.getPatients();
+
   }
 
   addPatient(): void {
@@ -39,18 +41,39 @@ export class PatientsComponent implements OnInit {
 
   getPatients() {
     this.patientService.getPatients().subscribe(
-      result => {
+      (result: Array<any>) => {
+        this.temp = [...result];
         this.rows = result;
         this.loadingIndicator = false;
+        this.columnsWithSearch = Object.keys(this.rows[0]);
       }
     );
   }
 
   onActivate(event) {
     if (event.type === 'click') {
-      console.log(event.row);
       this.router.navigate(['/patients/', event.row.id]);
     }
+  }
+
+  updateFilter(event) {
+    const filter = event.target.value.toLowerCase();
+
+    // assign filtered matches to the active datatable
+    this.rows = this.temp.filter(item => {
+      // iterate through each row's column data
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < this.columnsWithSearch.length; i++) {
+        const colValue = item[this.columnsWithSearch[i]] ;
+        console.log(colValue);
+
+        // if no filter OR colvalue is NOT null AND contains the given filter
+        if (!filter || (!!colValue && colValue.toString().toLowerCase().indexOf(filter) !== -1)) {
+          // found match, return true to add to result set
+          return true;
+        }
+      }
+    });
   }
 
 }
